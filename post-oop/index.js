ff4a038c47e18839a112ece2a72d9413b2e3b295
nadsatt <yabybka@gmail.com>
2020-11-22 12:17:45 +0200

    removed redundant toString method overriding in subclasses (+2 squashed commits) Squashed commits: [edefb30] removed redundant toString method invocation [7071d3a] made FriendsManager instance property of Inhabitant instance

--- post-oop/index.js
+++ post-oop/index.js
@@ -8,56 +8,39 @@
 
 // ======== OBJECTS DEFINITIONS ========
 
-/* WHAT WAS DONE to make my code SOLID:
-   - FOR S: i created new methods 'addFriends' and 'getFriends' in Inhabitant class and moved it to separate class 'FriendsManager'
-            i also wanted to move 'toString' method to separate class 'InhabitantConverter 
-            but i'm not sure about both ideas because it makes my code strange a bit (especially when i need to create FriendsManager instance for each inhabitant)
-   - FOR O: i changed 'toString' method in 'Inhabitant' to make it dependent on specific properties 
-            to have ability to redefine this method in subclasses
-            to show how this method can be simultaneously closed for modifications in superclass and open for modifications in subclasses
-   - FOR L: as i see, there is no cases where subclass can't be used instead of its superclass
-            for example, in 'addFriends' method of 'FriendsManager' class instance of any subclass can be used
-   - FOR I: as i see, there is no unused/unrelevant superclass' methods/props in subclasses
-   - FOR D: -
-*/ 
+class FriendsManager {
+   constructor(friends = []){
+      this.friends = friends;
+   }
+
+   addFriends(...friends){
+      this.friends.push(...friends);
+   }
+
+   getFriends(){
+      return this.friends.map(friend => friend.name).join(' ') || 'no friends';
+   }
+}
 
 class Inhabitant {
-   constructor(species, saying, gender, name, friends = []){
+   constructor(species, saying, gender, name, friends){
+      this.friendsManager = new FriendsManager(friends);
       this.species = species;
       this.saying = saying;
       this.gender = gender;
       this.name = name;
-      this.friends = friends;
    }
 
    toString(){
-      return `species: ${this.species}, saying: ${this.saying}, gender: ${this.gender}, name: ${this.name}, friends: ${new FriendsManager(this).getFriends()}`;
-   }
-}
-
-class FriendsManager {
-   constructor(inhabitant){
-      this.inhabitant = inhabitant;
-   }
-
-   addFriends(...friends){
-      this.inhabitant.friends.push(...friends);
-   }
-
-   getFriends(){
-      return this.inhabitant.friends.map(friend => friend.name).join(' ') || 'no friends';
+      return Object.entries(this).slice(1).map(([key, value]) => `${key}: ${value}`).join(', ') + `, friends: ${this.friendsManager.getFriends()}`;
    }
 }
 
 class Primate extends Inhabitant {
    constructor(species, saying, gender, name, ...friends){
       super(species, saying, gender, name, friends);
-      this.hands = 2;
       this.legs = 2;
-   }
-
-   toString(){
-      return super.toString() + `, legs: ${this.legs}, hands: ${this.hands}`;
+      this.hands = 2;
    }
 }
 
@@ -84,10 +67,6 @@ class Predator extends Inhabitant {
       super(species, saying, gender, name, friends);
       this.legs = 4;
    }
-
-   toString(){
-      return super.toString() + `, legs: ${this.legs}`;
-   }
 }
 
 class Dog extends Predator {
@@ -109,27 +88,18 @@ class CatWoman extends Cat {
       this.hands = 2;
       this.legs = 2;
    }
-
-   toString(){
-      return super.toString() + `, hands: ${this.hands}`;
-   }
 }
 
 const cat1 = new Cat('female', 'Kitty');
 const dog1 = new Dog('male', 'Bob', cat1);
-const man1 = new Man('Nick');
+const man1 = new Man('Nick'); 
 const woman1 = new Woman('Lucy');
 const woman2 = new Human('i\'m a special woman!', 'female', 'Linda');
 const catWoman1 = new CatWoman('Kate', man1, woman2);
 
-const cat1Friends = new FriendsManager(cat1);
-cat1Friends.addFriends(dog1);
-
-const man1Friends = new FriendsManager(man1);
-man1Friends.addFriends(catWoman1);
-
-const woman2Friends = new FriendsManager(woman2);
-woman2Friends.addFriends(catWoman1);
+cat1.friendsManager.addFriends(dog1);
+man1.friendsManager.addFriends(catWoman1);
+woman2.friendsManager.addFriends(catWoman1);
 
 const inhabitants = [
    man1, 
@@ -149,4 +119,4 @@ const inhabitants = [
    so code reviewers might focus on a single file that is index.js.
 */
 
-inhabitants.forEach(inhabitant => print(inhabitant.toString()));
+inhabitants.forEach(inhabitant => print(inhabitant));
