8dfa6d607f204da67c33dfaadff9b0149d9a2c17
nadsatt <yabybka@gmail.com>
2021-01-14 16:57:49 +0200

    finished 3rd friends app review

--- friends-app/main.js
+++ friends-app/main.js
@@ -1,49 +1,41 @@
 import { ApiService } from './modules/api-service.js';
 import { UserService } from './modules/user-service.js';
-
 import { LoadingPage } from './modules/loading-page.js';
 import { Header } from './modules/header.js';
 import { UserCardList } from './modules/user-card-list.js';
-import { PageLinkList } from './modules/page-link-list.js';
-import { FilterGroup } from './modules/filter-group.js';
+import { Pagination } from './modules/pagination.js';
+import { StateKeeperGroup } from './modules/state-keeper-group.js';
 
 class Program {
     constructor(){
-        this.defineServices();
-        this.defineComponents();
-    }
-
-    defineServices(){
         this.apiService = new ApiService();
         this.userService = new UserService(this.apiService);
-    }
 
-    defineComponents(){
         this.loadingPage = new LoadingPage(this);
-        document.body
-            .append(this.loadingPage.element);
+        document.body.append(this.loadingPage.element);
 
         this.header = new Header();
         document.body.querySelector('.header-wrapper')
             .append(this.header.element);
 
-        this.userCardList = new UserCardList(this.userService);
+        this.userCardList = new UserCardList();
         document.body.querySelector('.user-card-list-wrapper')
             .append(this.userCardList.element);
 
-        this.pageLinkList = new PageLinkList(this.userService, this.userCardList);
-        document.body.querySelector('.page-link-list-wrapper')
-            .append(this.pageLinkList.element);
+        this.pagination = new Pagination(this.userService, this.userCardList);
+        document.body.querySelector('.pagination-wrapper')
+            .append(this.pagination.element);
 
-        this.filterGroup = new FilterGroup(this.userService, this.pageLinkList);
-        document.body.querySelector('.filter-group-wrapper')
-            .append(this.filterGroup.element);
+        this.stateKeeperGroup = new StateKeeperGroup(
+            this.userService, this.pagination);
+        document.body.querySelector('.state-keeper-group-wrapper')
+            .append(this.stateKeeperGroup.element);
     }
 
     getUsers(usersCount = 100){
-        this.userService.getUsers(usersCount)
+        this.userService.fetchUsers(usersCount)
             .then(() => {
-                this.pageLinkList.performPagination();
+                this.pagination.performPagination();
                 this.loadingPage.remove();
             })
             .catch((e) => {
--- friends-app/modules/user-service.js
+++ friends-app/modules/user-service.js
@@ -1,12 +1,23 @@
 export class UserService {
     constructor(apiService){
         this.apiService = apiService;
-        this.originalUsers;
-        this.users;
-        this.currentPageUsers;
     }
 
-    async getUsers(usersCount){
+    get users(){
+        return this._users;
+    }
+    set users(value){
+        this._users = value;
+    }
+
+    get currentPageUsers(){
+        return this._currentPageUsers;
+    }
+    set currentPageUsers(value){
+        this._currentPageUsers = value;
+    }
+
+    async fetchUsers(usersCount){
         this.originalUsers = await this.apiService.getUsers(usersCount);
         this.users = [...this.originalUsers];
     }
